c715bcb9d90f8010b957c3b6ed809325ebf5dc2b
Rajeshdodapaneni <167109550+Rajeshdodapaneni@users.noreply.github.com>
2025-11-29 12:21:59 +0530

    Update age.js

--- age.js
+++ age.js
@@ -1,10 +1,23 @@
-function calculate(e){
+function CalculateAge(e){
     e.preventDefault()
-    let nm=document.getElementsByName('nm')[0].value
-    let dob=document.getElementsByName('dob')[0].value;
-    dob=String(dob).substring(6,10)
-    let cdate=new Date()
-    age=cdate.getFullYear()-dob
-    document.getElementById('agename').innerHTML=`Your name is ${nm} and age is ${age}`
+    let birthDate = document.getElementById("db").value;
+    let dob = new Date(birthDate);
+    let today = new Date();
 
+    let years = today.getFullYear() - dob.getFullYear();
+    let months = today.getMonth() - dob.getMonth();
+    let days = today.getDate() - dob.getDate();
+
+    if (days < 0) {
+        months--;
+        days += new Number(Date(today.getFullYear(), today.getMonth(), 0).getDate());
+    }
+
+    if (months < 0) {
+        years--;
+        months += 12;
+    }
+
+    document.getElementById("result").innerHTML =
+        `You are ${years} years, ${months} months, and ${days} days old.`;
 }
